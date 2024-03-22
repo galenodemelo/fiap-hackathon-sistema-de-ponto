0aa5640b4f9fcc7ebdd3f5199211ed36277c7636
Galeno de Melo <galenodemelo@gmail.com>
2024-03-22 07:41:08 -0300

    feat: Monta a política para autorizar ou negar a request do usuário

---
+++ handlers/src/main/java/br/com/fiap/authorizer/AuthPolicyBuilder.java
@@ -0,0 +1,36 @@
+package br.com.fiap.authorizer;
+
+import com.amazonaws.services.lambda.runtime.events.IamPolicyResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthPolicyBuilder {
+
+    public static IamPolicyResponse allow(String principalId, String resource) {
+        List<IamPolicyResponse.Statement> statementList = new ArrayList<>();
+        statementList.add(IamPolicyResponse.allowStatement(resource));
+
+        return IamPolicyResponse.builder()
+                .withPrincipalId(principalId)
+                .withPolicyDocument(buildPolicyDocument(statementList))
+                .build();
+    }
+
+    public static IamPolicyResponse deny(String resource) {
+        List<IamPolicyResponse.Statement> statementList = new ArrayList<>();
+        statementList.add(IamPolicyResponse.denyStatement(resource));
+
+        return IamPolicyResponse.builder()
+                .withPrincipalId("user")
+                .withPolicyDocument(buildPolicyDocument(statementList))
+                .build();
+    }
+
+    private static IamPolicyResponse.PolicyDocument buildPolicyDocument(List<IamPolicyResponse.Statement> statementList) {
+        return IamPolicyResponse.PolicyDocument.builder()
+                .withVersion(IamPolicyResponse.VERSION_2012_10_17)
+                .withStatement(statementList)
+                .build();
+    }
+}
